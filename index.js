ef57cd56f526a0d6c5cdfe4f573c078c361ea9e8
maxrazum <75961644+maxrazum@users.noreply.github.com>
2022-08-31 17:53:04 -0300

    connected mongoose

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/trekBikes', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongo connection open")
+    })
+    .catch(err => {
+        console.log("Mongo connection error")
+        console.log(err)
+    });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World')
+});
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000")
