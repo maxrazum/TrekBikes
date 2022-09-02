42e9a9723f294a1dd2f02cd496e1ac7a7d6bf6f8
maxrazum <75961644+maxrazum@users.noreply.github.com>
2022-09-02 15:53:30 -0300

    modified styling

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static('views'));
 
 const categories = ['mountain', 'hybrid', 'road'];
 
